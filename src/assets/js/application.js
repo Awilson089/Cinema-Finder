daf835dca0799c9074952e6f3ac2fb3af592846d
Adam Wilson <adamalexanderwilson@gmail.com>
2016-03-01 11:29:24 +0000

    Added autocomplete

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -1,34 +1,45 @@
 var map;
 var geocoder = null;
 var closest = [];
+var geo_markers = [];
+var latLng;
 
 function google_map() {
-
 	function initialize() {
 		geocoder = new google.maps.Geocoder();
 
 		map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 7,
-			center: new google.maps.LatLng(54.559322,-4.174804),
-			mapTypeId: google.maps.MapTypeId.TERRAIN
+			mapTypeId: google.maps.MapTypeId.TERRAIN,
+			LatLngBounds: (
+			new google.maps.LatLng(49.383639452689664, -17.39866406249996),
+			new google.maps.LatLng(59.53530451232491, 8.968523437500039)),
+			center: new google.maps.LatLng(54.559322,-4.174804)
 		});
+
+		var input = document.getElementById('address');
+		var options = {
+		   types: ['(cities)'],
+		   componentRestrictions: {country: 'uk'}//Turkey only
+		};
+		var autocomplete = new google.maps.places.Autocomplete(input, options);
 
 		var script = document.createElement('script');
 		script.src = 'assets/data/cinemas.geojsonp';
 		document.getElementsByTagName('head')[0].appendChild(script);
 	}
+
+
 	// Loop through the results array and place a marker for each
 	// set of coordinates.
-	window.cinemalist_callback = function(results) {
+	window.cinemalist_callback = function (results) {
 		for (var i = 0; i < results.cinemas.length; i++) {
-			var coords = results.cinemas[i].geometry.coordinates;
 			var icon = results.cinemas[i].properties.icon;
-
 			geocoder.geocode({
 		        'address': results.cinemas[i].properties.postcode
 		    }, function (results, status) {
 		        if (status == google.maps.GeocoderStatus.OK) {
-					var latLng = results[0].geometry.location;
+					latLng = results[0].geometry.location;
 
 					var marker = new google.maps.Marker({
 						position: latLng,
@@ -36,10 +47,11 @@ function google_map() {
 						map: map
 					});
 		        } else {
-		            alert('Geocode was not successful for the following reason: ' + status);
+		            alert("Geocode was not successful for the following reason:"+ status);
 		        }
 		   	});
 		}
+	   	sessionStorage.setItem('geo_markers', JSON.stringify(geo_markers));
 	}
 	google.maps.event.addDomListener(window, 'load', initialize)
 }
@@ -52,15 +64,18 @@ function convertAddress() {
         'address': address
     }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
+        	var pt = results[0].geometry.location;
+            map.setCenter(pt);
             map.setZoom(12);
 
 			if (typeof customerMarker != 'undefined') { customerMarker.setMap(null); }
 
             customerMarker = new google.maps.Marker({
                 map: map,
-                position: results[0].geometry.location
+                position: pt
             });
+
+            findClosestN(pt, 2);
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
@@ -86,4 +101,11 @@ function sortByDist(a, b) {
 
 $(document).ready(function() {
 	google_map();
+
+	$('#address').keypress(function (e) {
+	  if (e.which == 13) {
+	    convertAddress();
+	    return false
+	  }
+	});
 });
