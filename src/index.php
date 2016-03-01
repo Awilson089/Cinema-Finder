b31c04d3928b63a2001b32518d69225dd3af56e2
Adam Wilson <adamalexanderwilson@gmail.com>
2016-03-01 11:30:07 +0000

    Bootstrap classes added

--- src/index.php
+++ src/index.php
@@ -9,10 +9,14 @@
 	<div class="map-wrap">
 	    <div id="map"></div>
 		<div class="map-form">
-			<label for="location">Enter Your Location: </label>
-	   		<input type="text" id="address" name="location" value="Leeds"></input>
-			<input type="button" value="Search" onclick="convertAddress();"></input>
-		</div>
+			<form class="form-inline">
+				<div class="form-group">
+					<label for="location">Enter Your Location: </label>
+		   			<input type="text" id="address" name="location" value="Leeds" class="form-control">
+				</div>
+				<input type="submit" value="Search" onclick="convertAddress();" class="btn btn-primary">
+			</div>
+		</form>
 	</div>
 
 	<?php include('includes/footer.php') ?>
