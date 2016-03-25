3965303b0fb968d99a66bf776f6dbddd5f3eb4d2
Adam Wilson <adamalexanderwilson@gmail.com>
2016-03-25 16:40:28 +0000

    Added grid div

--- src/index.php
+++ src/index.php
@@ -8,18 +8,35 @@
 	<?php include('includes/nav.php') ?>
 	<div class="map-wrap">
 	    <div id="map"></div>
-		<div class="map-form">
-			<form id="location-form" class="form-inline">
-				<div class="form-group">
-					<label for="location">Enter Your Location: </label>
+
+		<div class="lander">
+			<div class="map-form">
+				<form id="location-form" class="form-inline">
 		   			<input type="text" id="address" name="location" value="Leeds" class="form-control">
+                    <a href="#" class="btn btn-primary location" onclick="use_location();"><span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span></a>
+					<button type="submit" onclick="convert_address();" class="btn btn-primary">Search</button>
 				</div>
-				<input type="submit" value="Search" onclick="convertAddress();" class="btn btn-primary">
+			</form>
+			<div class="grid image-grid">
+				<div class="grid-sizer"></div>
 			</div>
-		</form>
+		</div>
+
+		<div class="nearest-cinema">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Nearest Cinemas <span href="#" class="pull-right toggler"><i class="fa fa-chevron-down"></i></span></h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="data"></div>
+				</div>
+			</div>
+		</div>
 	</div>
 
-	<?php include('includes/footer.php') ?>
+	<div class="cinema-modal"></div>
 </body>
 </html>
 
