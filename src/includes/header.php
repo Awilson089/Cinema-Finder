964815fd7446c2c0c29047acc4bb96b39e1feac9
Adam Wilson <adamalexanderwilson@gmail.com>
2016-03-25 16:42:11 +0000

    added links and script tags

--- src/includes/header.php
+++ src/includes/header.php
@@ -16,9 +16,15 @@
 
     <script src="assets/components/jquery/dist/jquery.min.js"></script>
     <script src="assets/components/modernizr/modernizr.js"></script>
+    <script src="assets/components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="assets/components/parsleyjs/dist/parsley.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.2/isotope.pkgd.min.js"></script>
+    <script src="assets/js/packery.layout.min.js"></script>
+
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDF1hHftzt3qh9yfi68k1CCRUCG74L1TVs&libraries=places,geometry&sensor=false&region=GB&gl=gb" type="text/javascript"></script>
 
+    <script src="assets/js/image_grid.js"></script>
     <script src="assets/js/application.js"></script>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
