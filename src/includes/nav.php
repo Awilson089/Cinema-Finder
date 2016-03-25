63a98c6eb0a018539106b33e730b8b307df5f388
Adam Wilson <adamalexanderwilson@gmail.com>
2016-03-25 23:34:35 +0000

    Changed links to icons and added logo to nav

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -3,23 +3,26 @@
 	  <div class="container-fluid">
 	    <!-- Brand and toggle get grouped for better mobile display -->
 	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
 	        <span class="sr-only">Toggle navigation</span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	      </button>
-	      <a class="navbar-brand" href="#">Silver Screen</a>
+	      <a class="navbar-brand logo" href="#">Silver Screen</a>
 	    </div>
 
 	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-				<li><a href="#">About</a></li>
-			</ul>
+	    <div class="collapse navbar-collapse" id="main-nav">
 			<ul class="nav navbar-nav pull-right">
-	        	<li><a href="#">Login</a><li>
+				<li><a href="#"><i class="fa fa-question-circle"></i></a></li>
+				<li><a href="#" class="mr2"><i class="fa fa-user"></i></a></li>
+				<li>
+					<form id="nav-location-form" class="form-inline">
+			   			<input type="text" id="nav-address" name="location" class="form-control">
+						<button type="submit" class="btn btn-primary">Search</button>
+					</form>
+				</li>
 	        </ul>
 	    </div><!-- /.navbar-collapse -->
 	  </div><!-- /.container-fluid -->
