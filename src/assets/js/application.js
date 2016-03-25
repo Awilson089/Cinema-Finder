7a59f40635f688d88dd219c0215f8ead9866f475
Adam Wilson <adamalexanderwilson@gmail.com>
2016-03-25 16:39:08 +0000

    Added isotope and individual modals for each cinema

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -1,12 +1,18 @@
 var map;
 var geocoder = null;
 var closest = [];
+var test = [];
 var geo_markers = [];
-var all_cinemas = [];
+
+var infowindow = new google.maps.InfoWindow({});
 
 function google_map() {
 	function initialize() {
 		geocoder = new google.maps.Geocoder();
+
+		//https://snazzymaps.com/style/83/muted-blue
+		var styles = [{"featureType":"all","stylers":[{"saturation":0},{"hue":"#e7ecf0"}]},{"featureType":"road","stylers":[{"saturation":-70}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"simplified"},{"saturation":-60}]}];
+	  	var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
 
 		map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 7,
@@ -20,12 +26,18 @@ function google_map() {
 		var input = document.getElementById('address');
 		var options = {
 		   types: ['(cities)'],
-		   componentRestrictions: {country: 'uk'}//UK only
+		   componentRestrictions: {country: 'uk'} //UK only
 		};
 		var autocomplete = new google.maps.places.Autocomplete(input, options);
+ 		var infowindow = new google.maps.InfoWindow();
+    	var marker, i;
 
 		var script = document.createElement('script');
 		script.src = 'assets/data/cinemas.geojsonp';
+
+		map.mapTypes.set('map_style', styledMap);
+  		map.setMapTypeId('map_style');
+
 		document.getElementsByTagName('head')[0].appendChild(script);
 	}
 
@@ -33,24 +45,76 @@ function google_map() {
 	// Loop through the results array and place a marker for each
 	// set of coordinates.
 	window.cinemalist_callback = function(results) {
-		all_cinemas = results;
+		var all_cinemas = [];
+
 		for (var i = 0; i < results.cinemas.length; i++) {
 			var icon = results.cinemas[i].properties.icon;
 			var latLng = results.cinemas[i].geometry.location;
-			console.log(latLng);
+
+            var url = results.cinemas[i].properties.url;
+            var id = results.cinemas[i].id;
+
+            var stripped_url = url.replace('http://','','/');
+            var stripped_url = stripped_url.replace('/','');
 
 			var marker = new google.maps.Marker({
 				position: latLng,
 				icon: icon,
-				map: map
+				map: map,
+				clickable: true
 			});
 			geo_markers.push(marker);
+			all_cinemas.push(results.cinemas[i]);
+
+			marker.info = new google.maps.InfoWindow({
+			  	content:    '<div class="info-window"><h4><strong>'+results.cinemas[i].properties.name+'</strong></h4>'
+        			  		+'<p>'+results.cinemas[i].properties.address+'</p>'
+                            +'<p><a href="'+url+'" target="_blank">'+stripped_url+'</a></p>'
+                            +'<div class="row">'+
+				        		'<div class="col-sm-4">'+
+				        			'<a href="#" onclick="cinema_history('+id+');  return false;" class="btn btn-primary mt1">View Info</a>'+
+				        		'</div>'
+			});
+
+			google.maps.event.addListener(marker, 'click', function() {
+    			this.info.open(map, this);
+			});
 		}
+		sessionStorage.setItem('all_cinemas', JSON.stringify(all_cinemas));
 	}
 	google.maps.event.addDomListener(window, 'load', initialize)
 }
 
-function convertAddress() {
+function save_nearest_cinemas(pt) {
+	$('.image-grid').hide();
+
+    map.setCenter(pt);
+    map.setZoom(12);
+
+    if (typeof customerMarker != 'undefined') { customerMarker.setMap(null); }
+
+    customerMarker = new google.maps.Marker({
+        map: map,
+        position: pt
+    });
+
+    closest = find_closest_n(pt, 3);
+    var near_cinemas = [];
+
+    for (var i = 0; i < closest.length; i++) {
+        var distance = '<span class="glyphicon glyphicon-map-marker mr1" aria-hidden="true"></span>' + Math.ceil(closest[i].distance * 0.000621371192) + ' miles away';
+
+        near_cinemas.push(closest[i].info.content +
+    		 '<div class="col-sm-8">'+
+    		 	'<p class="distance mt2 mb0">' + distance + '<p>'+
+    		 '</div>'+
+		 '</div>');
+    }
+    sessionStorage.setItem('near_cinemas', JSON.stringify(near_cinemas));
+    show_nearest_cinemas();
+}
+
+function convert_address() {
 	$('.map-form').fadeOut(400);
 
     var address = document.getElementById('address').value;
@@ -59,62 +123,138 @@ function convertAddress() {
     }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
         	var pt = results[0].geometry.location;
-            map.setCenter(pt);
-            map.setZoom(12);
 
-			if (typeof customerMarker != 'undefined') { customerMarker.setMap(null); }
-
-            customerMarker = new google.maps.Marker({
-                map: map,
-                position: pt
-            });
-
-            findClosestN(pt, 3);
+            save_nearest_cinemas(pt);
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
     });
 }
 
-function findClosestN(pt, numberOfResults) {
+function use_location() {
+	$('.map-form').fadeOut(400);
+
+    navigator.geolocation.getCurrentPosition(success);
+
+    function success(position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        var pt  = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        save_nearest_cinemas(pt);
+    }
+}
+
+function find_closest_n(pt, number_of_results) {
     var closest = [];
-    console.log(geo_markers);
     for (var i = 0; i < geo_markers.length; i++) {
         geo_markers[i].distance = google.maps.geometry.spherical.computeDistanceBetween(pt, geo_markers[i].getPosition());
         closest.push(geo_markers[i]);
     }
-    closest.sort(sortByDist);
-    var near_cinemas = [];
-    var near_cinema = [];
-    near_cinemas = closest.splice(0,numberOfResults);
+    closest.sort(sort_by_dist);
+    return closest.splice(0,number_of_results);
 
-    for (var i = 0; i < near_cinemas.length; i++) {
-        near_cinema.push(near_cinemas[i].getPosition());
-    }
-    sessionStorage.setItem('near_cinemas', near_cinema);
+    // var near_cinemas = [];
+    // var near_cinema = [];
+    // near_cinemas = closest.splice(0,number_of_results);
 
- 	window.cinemalist_callback = function(results) {
-	 	all_cinemas = results;
-		for (var i = 0; i < results.cinemas.length; i++) {
-			for (var i = 0; i < sessionStorage.near_cinema.length; i++) {
-				if(results.cinemas[i].geometry.location == sessionStorage.near_cinema[i]){
-					console.log('success')
-				}
-			}
-		}
-	}
+    // for (var i = 0; i < near_cinemas.length; i++) {
+    //     near_cinema.push(near_cinemas[i].getPosition());
+    // }
+    // sessionStorage.setItem('near_cinemas', JSON.stringify(near_cinema));
+
+	//for (var i = 0; i < sessionStorage.all_cinemas.length; i++) {
+	// 	for (var j = 0; j < sessionStorage.near_cinema.length; j++) {
+	// 		console.log(sessionStorage.all_cinemas[i]);
+	// 		console.log(sessionStorage.near_cinema[j]);
+
+	// 		if(results.cinemas[i].geometry.location != sessionStorage.near_cinema[j]){
+	// 			console.log('success');
+	// 		}
+	// 	}
+	// }
 }
 
-function sortByDist(a, b) {
+function sort_by_dist(a, b) {
     return (a.distance - b.distance)
+}
+
+function show_nearest_cinemas() {
+    $('.nearest-cinema').fadeIn(400);
+    var nearest_cinema_html = [];
+
+    nearest_cinema_html = sessionStorage.getItem('near_cinemas');
+    nearest_cinema_parsed = JSON.parse(nearest_cinema_html);
+    for (var i = 0; i < nearest_cinema_parsed.length; i++) {
+	    $('<div class="col-sm-4 col-md-3"><div class="cinema">'+nearest_cinema_parsed[i]+'</div></div>').appendTo(".data");
+    }
+}
+
+function cinema_history(id) {
+	var all_cinemas = [];
+
+    all_cinemas = sessionStorage.getItem('all_cinemas');
+	var all_cinemas_parsed = JSON.parse(all_cinemas);
+
+	// Loop through the results array and place a marker for each
+	// set of coordinates.
+	for (var i = 0; i < all_cinemas_parsed.length; i++) {
+		if(all_cinemas_parsed[i].id == id){
+			var img_url = all_cinemas_parsed[i].properties.name;
+			var img_url = img_url.replace(' ','_');
+			var img_url = img_url.toLowerCase();
+
+			$(".cinema-modal").append("<div class='modal fade' id='cinema_modal' tabindex='-1' role='dialog' aria-labelledby='cinema-modal-label'>"+
+				  "<div class='modal-dialog' role='document'>"+
+				    "<div class='modal-content'>"+
+				      "<div class='modal-header'>"+
+				        "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+
+				        "<h4 class='modal-title' id='cinema-modal-label'>"+all_cinemas_parsed[i].properties.name+"</h4>"+
+				      "</div>"+
+				      "<div class='modal-body'>"+
+				      		"<div class='row'>"+
+					      		"<div class='col-sm-6'>"+
+					      			"<img src='assets/img/cinemas/"+img_url+".jpg' class='img-responsive'>"+
+					      		"</div>"+
+					      		"<div class='col-sm-6'>"+
+						   			"<p>"+all_cinemas_parsed[i].properties.address+"</p>"+
+						   			"<p>"+all_cinemas_parsed[i].properties.city+"</p>"+
+						   			"<p>"+all_cinemas_parsed[i].properties.postcode+"</p>"+
+						   			"<p><a href='"+all_cinemas_parsed[i].properties.url+"' target='_blank'>"+all_cinemas_parsed[i].properties.url+"</a></p>"+
+						   			"<p>Status: "+all_cinemas_parsed[i].properties.status+"</p>"+
+						   			"<p>Open Since: "+all_cinemas_parsed[i].properties.opened+"</p>"+
+					      		"</div>"+
+							"</div>"+
+						"</div>"+
+						"<div class='modal-footer'>"+
+						"	<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>"+
+						"</div>"+
+				    "</div>"+
+				  "</div>"+
+				"</div>");
+
+			$('#cinema_modal').modal('show');
+
+			$('#cinema_modal').on('hidden.bs.modal', function () {
+			  $('#cinema_modal').remove();
+			});
+		}
+	}
+
+	return false;
 }
 
 $(document).ready(function() {
 	google_map();
 
+	$('.toggler').click(function () {
+		$('.nearest-cinema').toggleClass("down");
+		$(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
+	});
+
 	$('#address').keypress(function (e) {
 		if (e.which == 13) {
-			convertAddress();
+			convert_address();
 			return false
 		}
 	});
---
+++ src/assets/js/image_grid.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	var folder = "assets/img/cinemas/";
+
+	$.ajax({
+	    url : folder,
+	    success: function (data) {
+	        $(data).find("a").attr("href", function (i, val) {
+	            if( val.match(/\.(jpe?g|png|gif)$/) ) {
+	                $(".grid").append( "<div class='grid-item' style='background: url("+ folder + val +") center center no-repeat; background-size: cover; background-color: #666; background-blend-mode: multiply;' class='img-responsive'></div>" );
+	            }
+	        });
+	    }
+	});
+
+	$('.grid').isotope({
+		layoutMode: 'packery',
+		packery: {
+		  columnWidth: '.grid-sizer'
+		},
+		itemSelector: '.grid-item',
+		percentPosition: true
+	});
+});
